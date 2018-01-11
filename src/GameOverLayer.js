bbfcbfc4cf10aa650afd0a261878774d1778a4ce
Core-Ventura <core.ventura@gmail.com>
2018-01-11 18:53:26 +0000

    Second Commit

--- src/GameOverLayer.js
+++ src/GameOverLayer.js
@@ -1,6 +1,6 @@
 var GameOverLayer = cc.LayerColor.extend({
     ctor:function(){
-        this._super(cc.color(0,0,0,180));
+        this._super(cc.color(0,22,0,180));
         var winSize = cc.director.getWinSize();
 
         var botonReiniciar  = new cc.MenuItemSprite(
@@ -9,7 +9,7 @@ var GameOverLayer = cc.LayerColor.extend({
             this.pulsarReiniciar, this);
 
         var menu = new cc.Menu(botonReiniciar);
-        menu.setPosition(winSize.width/2, winSize.heigth/2);
+        menu.setPosition(winSize.width/2, winSize.height/2);
         this.addChild(menu);
 
     }, pulsarReiniciar:function () {
--- src/app.js
+++ src/app.js
@@ -5,8 +5,13 @@ var GameLayer = cc.Layer.extend({
     velocidadY:null,
     spriteCoche: null,
     spriteBarra:null,
-    spriteBarraIA: null,
+    spriteBarraIA:null,
     dificultad:null,
+    scorePlayer:null,
+    labelScorePlayer:null,
+    scoreIA:null,
+    labelScoreIA:null,
+    scoreToWin:null,
     ctor:function () {
         this._super();
         var size = cc.winSize;
@@ -16,11 +21,18 @@ var GameLayer = cc.Layer.extend({
         // 2 = Normal
         // 3 = Difícil
         // 4 = Imposible
-        this.dificultad = 3;
-        this.velocidadX = 6;
+        this.dificultad = 2;
+
+        // Velocidades
+        this.velocidadX = 4;
         this.velocidadY = 4;
 
-        //Fondo
+        // Puntuaciones
+        this.scorePlayer = 0;
+        this.scoreIA = 0;
+        this.scoreToWin = 3;
+
+        // Fondo
         var spriteFondo = cc.Sprite.create(res.fondo_png);
         spriteFondo.setPosition(cc.p(size.width/2, size.height/2));
         spriteFondo.setScale(size.width / spriteFondo.width);
@@ -31,18 +43,29 @@ var GameLayer = cc.Layer.extend({
         this.spritePelota.setPosition( cc.p( size.width/2 , size.height/2 )  );
         this.addChild(this.spritePelota);
 
-        // Barra aliada
+        // Barra jugador
         this.spriteBarra = cc.Sprite.create(res.barra_2_png);
         this.spriteBarra.setPosition(cc.p(size.width*0.1, size.height*0.5 ));
         this.spriteBarra.rotation = 90;
         this.addChild(this.spriteBarra);
 
-        // Barra enemiga
+        // Barra IA
         this.spriteBarraIA = cc.Sprite.create(res.barra_2_png);
         this.spriteBarraIA.setPosition(cc.p(size.width*0.9, size.height*0.5 ));
         this.spriteBarraIA.rotation = 90;
         this.addChild(this.spriteBarraIA);
 
+        // Puntuación jugador
+        this.labelScorePlayer = new cc.LabelTTF(this.scorePlayer.toString());
+        this.labelScorePlayer.setFontSize(80);
+        this.labelScorePlayer.setPosition(cc.p(size.width*0.4, size.height*0.9));
+        this.addChild(this.labelScorePlayer);
+
+        // Puntuación IA
+        this.labelScoreIA = new cc.LabelTTF(this.scoreIA.toString());
+        this.labelScoreIA.setFontSize(80);
+        this.labelScoreIA.setPosition(cc.p(size.width*0.6, size.height*0.9));
+        this.addChild(this.labelScoreIA);
 
         cc.eventManager.addListener({
             event: cc.EventListener.MOUSE,
@@ -68,10 +91,10 @@ var GameLayer = cc.Layer.extend({
          var instancia = event.getCurrentTarget();
          console.log(keyCode);
          if ( keyCode == 38 && instancia.spriteBarra.y < 400){ 
-            instancia.spriteBarra.y =  instancia.spriteBarra.y + 10;
+            instancia.spriteBarra.y =  instancia.spriteBarra.y + 14;
          }
          if ( keyCode == 40 && instancia.spriteBarra.y > 50){
-            instancia.spriteBarra.y =  instancia.spriteBarra.y - 10;
+            instancia.spriteBarra.y =  instancia.spriteBarra.y - 14;
          }
 
     }, procesarMouseDown: function ( event ){
@@ -84,11 +107,19 @@ var GameLayer = cc.Layer.extend({
 
     }, update: function(){
 
-        //Mover pelota
+
+        // Comprobar GAME OVER
+        if (this.scorePlayer == this.scoreToWin || this.scoreIA == this.scoreToWin){
+            cc.audioEngine.stopMusic();
+            cc.director.pause();
+            this.addChild(new GameOverLayer);
+        }
+
+        // Mover pelota
         this.spritePelota.x = this.spritePelota.x + this.velocidadX;
         this.spritePelota.y = this.spritePelota.y + this.velocidadY;
 
-        //Mover barra enemiga
+        // Mover barra enemiga
         if (this.spriteBarraIA.y < 400 && this.spritePelota.y > this.spriteBarraIA.y){
             this.spriteBarraIA.y = this.spriteBarraIA.y + this.dificultad;
         } else if (this.spriteBarraIA.y > 50 && this.spritePelota.y < this.spriteBarraIA.y){
@@ -96,11 +127,23 @@ var GameLayer = cc.Layer.extend({
         }
 
         if (this.spritePelota.x < 0){
-            this.spritePelota.x = 0;
+            // Aumentamos la puntuación
+            this.scoreIA += 1;
+            this.labelScoreIA.setString(this.scoreIA.toString());
+            // Reseteamos la posición de la pelota y la lanzamos en la dirección contraria
+            this.spritePelota.x = cc.winSize.width/2;
+            this.spritePelota.y = cc.winSize.height/2;
             this.velocidadX = this.velocidadX*-1;
+
         }
+
         if (this.spritePelota.x > cc.winSize.width){
-            this.spritePelota.x = cc.winSize.width;
+            // Aumentamos la puntuación
+            this.scorePlayer += 1;
+            this.labelScorePlayer.setString(this.scorePlayer.toString());
+            // Reseteamos la posición de la pelota y la lanzamos en la dirección contraria
+            this.spritePelota.x = cc.winSize.width/2;
+            this.spritePelota.y = cc.winSize.height/2;
             this.velocidadX = this.velocidadX*-1;
         }
         if (this.spritePelota.y < 0){
@@ -112,7 +155,7 @@ var GameLayer = cc.Layer.extend({
             this.velocidadY = this.velocidadY*-1;
         }
 
-       //Colisiones
+       // Colisiones
         var areaPelota = this.spritePelota.getBoundingBox();
         var areaBarra = this.spriteBarra.getBoundingBox();
         var areaBarraIA = this.spriteBarraIA.getBoundingBox();
@@ -120,6 +163,7 @@ var GameLayer = cc.Layer.extend({
         if(cc.rectIntersectsRect(areaPelota, areaBarra) || 
             cc.rectIntersectsRect(areaPelota, areaBarraIA)){
                 console.log("Collision");
+                cc.audioEngine.playEffect(res.grunt_wav);
                 this.velocidadX = this.velocidadX*-1;
         }
     }
@@ -129,7 +173,9 @@ var GameLayer = cc.Layer.extend({
 var GameScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-        // Hasta aqui lo comun
+        // Hasta aqui lo común
+        cc.director.resume();
+        cc.audioEngine.playMusic(res.sonidobucle_wav, true);
         var layer = new GameLayer();
         this.addChild(layer);
     }
